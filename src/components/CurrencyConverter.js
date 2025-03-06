eb0a0973f515ab579c8b999818f4f5c9b53b4866
dima <>
2025-03-06 17:37:52 +0200

    Добавлены  флаги

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import "../styles.css";
 import { FaExchangeAlt } from "react-icons/fa"; // Иконка для кнопки обмена валютами
 import { FaSpinner } from "react-icons/fa"; // Иконка для спиннера загрузки
-import Flag from "react-world-flags"; // Импорт флага валюты
+import currencyFlags from "../components/currencyFlags.json"; // Флаги валют
 
 const CurrencyConverter = () => {
     const { t, i18n } = useTranslation();
@@ -16,7 +16,6 @@ const CurrencyConverter = () => {
     const [loading, setLoading] = useState(true);
     const [theme, setTheme] = useState("light");
 
-    // Запрашиваем курсы валют с базовой валютой fromCurrency
     useEffect(() => {
         if (!fromCurrency) return;
         setLoading(true);
@@ -30,13 +29,12 @@ const CurrencyConverter = () => {
                     throw new Error("Invalid data");
                 }
             })
-            .catch((err) => {
+            .catch(() => {
                 setError(t("errorMessage"));
                 setLoading(false);
             });
     }, [fromCurrency, t]);
 
-    // Пересчитываем сумму при изменении amount, fromCurrency, toCurrency или exchangeRates
     useEffect(() => {
         if (exchangeRates[toCurrency] && amount !== "") {
             const newConvertedAmount = (amount * exchangeRates[toCurrency]).toFixed(2);
@@ -46,13 +44,11 @@ const CurrencyConverter = () => {
         }
     }, [amount, fromCurrency, toCurrency, exchangeRates]);
 
-    // Обработчик обмена валютами
     const handleSwapCurrencies = () => {
         setFromCurrency(toCurrency);
         setToCurrency(fromCurrency);
     };
 
-    // Обработчик переключения темы
     const handleThemeToggle = () => {
         setTheme(theme === "light" ? "dark" : "light");
     };
@@ -67,7 +63,6 @@ const CurrencyConverter = () => {
                     type="number"
                     value={amount}
                     onChange={(e) => {
-                        // Разрешаем ввод только положительных чисел или пустую строку
                         if (e.target.value >= 0 || e.target.value === "") {
                             setAmount(e.target.value);
                         }
@@ -82,20 +77,16 @@ const CurrencyConverter = () => {
             <div className="currency-selection">
                 <div className="currency-label">{t("fromCurrency")}</div>
                 <div className="currency-with-flag">
-                    <Flag code={fromCurrency} alt={fromCurrency} style={{ width: "20px", height: "15px" }} />
+                    <img src={currencyFlags[fromCurrency]} alt={fromCurrency} className="flag-icon" />
                     <select
                         value={fromCurrency}
                         onChange={(e) => setFromCurrency(e.target.value)}
                         className="select-field"
                         disabled={loading}
                     >
-                        <option value="UAH">{t("UAH")}</option>
-                        <option value="USD">{t("USD")}</option>
-                        <option value="EUR">{t("EUR")}</option>
-                        <option value="GBP">{t("GBP")}</option>
-                        <option value="JPY">{t("JPY")}</option>
-                        <option value="CHF">{t("CHF")}</option>
-                        <option value="AUD">{t("AUD")}</option>
+                        {Object.keys(currencyFlags).map((currency) => (
+                            <option key={currency} value={currency}>{t(currency)}</option>
+                        ))}
                     </select>
                 </div>
 
@@ -105,31 +96,22 @@ const CurrencyConverter = () => {
 
                 <div className="currency-label">{t("toCurrency")}</div>
                 <div className="currency-with-flag">
-                    <Flag code={toCurrency} alt={toCurrency} style={{ width: "20px", height: "15px" }} />
+                    <img src={currencyFlags[toCurrency]} alt={toCurrency} className="flag-icon" />
                     <select
                         value={toCurrency}
                         onChange={(e) => setToCurrency(e.target.value)}
                         className="select-field"
                         disabled={loading}
                     >
-                        <option value="UAH">{t("UAH")}</option>
-                        <option value="USD">{t("USD")}</option>
-                        <option value="EUR">{t("EUR")}</option>
-                        <option value="GBP">{t("GBP")}</option>
-                        <option value="JPY">{t("JPY")}</option>
-                        <option value="CHF">{t("CHF")}</option>
-                        <option value="AUD">{t("AUD")}</option>
+                        {Object.keys(currencyFlags).map((currency) => (
+                            <option key={currency} value={currency}>{t(currency)}</option>
+                        ))}
                     </select>
                 </div>
             </div>
 
             <h3>
-                {t("result")}:{" "}
-                {convertedAmount
-                    ? `${convertedAmount} ${toCurrency}`
-                    : loading
-                        ? <FaSpinner className="spinner" />
-                        : "..."}
+                {t("result")}: {convertedAmount ? `${convertedAmount} ${toCurrency}` : loading ? <FaSpinner className="spinner" /> : "..."}
             </h3>
 
             {exchangeRates[toCurrency] && (
